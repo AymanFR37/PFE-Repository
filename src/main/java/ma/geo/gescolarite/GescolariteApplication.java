abf5015acb5c47fed2aed71f4a167f2905517b90
AymanFR37 <ayman.farik@gmail.com>
2023-06-07 11:46:59 +0100

    first init

---
+++ src/main/java/ma/geo/gescolarite/GescolariteApplication.java
@@ -0,0 +1,13 @@
+package ma.geo.gescolarite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GescolariteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GescolariteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/geo/gescolarite/GescolariteApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.geo.gescolarite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GescolariteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
